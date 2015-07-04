8989dbd95a88c1105b852ef59ee3fd70279de215
Robert M. Lefkowitz <r0ml@mac.com>
2015-07-04 14:47:59 -0400

    need a special .ghci for when running ghci

--- Preface/StrUtils.hs
+++ Preface/StrUtils.hs
@@ -1,7 +1,7 @@
 
-module Preface.StrUtils 
-
-where
+module Preface.StrUtils (
+  shell_escape
+) where
 
 shell_escape :: String -> String
 shell_escape = concat . ("'" : ) . (:["'"]) . concatMap esq where esq c = if c == '\'' then "'\"'\"'" else [c]
